e0454220e29200f31abfd747626ccca900cab4e7
raulinf-prog <raulinf@iesmatematicvicentcasellescosta.com>
2026-07-27 10:07:52 +0200

    Update auto_plus.py

--- auto_plus.py
+++ auto_plus.py
@@ -1,75 +1,20 @@
 import requests
-from bs4 import BeautifulSoup
 import os
+
 print("🚀 ESTOY EJECUTANDO ESTE ARCHIVO")
-URL = "https://www.mintur.gob.es/PortalAyudas/programa-auto/Paginas/Index.aspx"
 
 TOKEN = os.environ["TELEGRAM_TOKEN"]
 CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 
-url2 = "https://api.telegram.org/bot{TOKEN}/sendMessage"
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
 respuesta = requests.post(
-    url2,
+    url,
     data={
         "chat_id": CHAT_ID,
-        "text": "✅ Prueba: el avisador Auto+ funciona"
+        "text": "✅ Prueba: GitHub Actions + Telegram funciona"
     }
 )
 
 print("Código:", respuesta.status_code)
 print("Respuesta Telegram:", respuesta.text)
-
-
-def enviar_telegram(texto):
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-
-    requests.post(
-        url,
-        data={
-            "chat_id": CHAT_ID,
-            "text": texto
-        }
-    )
-
-
-def comprobar():
-
-    r = requests.get(
-        URL,
-        timeout=20
-    )
-
-    texto = BeautifulSoup(
-        r.text,
-        "html.parser"
-    ).get_text().lower()
-
-    palabras = [
-        "solicitud",
-        "plazo abierto",
-        "presentación",
-        "abierto"
-    ]
-
-    for palabra in palabras:
-        if palabra in texto:
-            return True
-
-    return False
-
-
-if comprobar():
-
-    enviar_telegram(
-        "🚗 AUTO+ PUEDE ESTAR ABIERTO\n\n"
-        "Revisa la convocatoria:"
-        "\n" + URL
-    )
-
-else:
-    print("Todavía cerrado")
-    enviar_telegram(
-        "🚗 AUTO+ PUEDE ESTAR ABIERTO\n\n"
-        "Revisa la convocatoria:"
-        "\n" + URL)
