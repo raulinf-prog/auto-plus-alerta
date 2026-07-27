da5021019ebc1b80390cd7132fabe1b31aaed88b
raulinf-prog <raulinf@iesmatematicvicentcasellescosta.com>
2026-07-27 09:32:16 +0200

    Create auto_plus.py

---
+++ auto_plus.py
@@ -0,0 +1,58 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+
+URL = "https://www.mintur.gob.es/PortalAyudas/programa-auto/Paginas/Index.aspx"
+
+TOKEN = os.environ["TELEGRAM_TOKEN"]
+CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
+
+
+def enviar_telegram(texto):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+    requests.post(
+        url,
+        data={
+            "chat_id": CHAT_ID,
+            "text": texto
+        }
+    )
+
+
+def comprobar():
+
+    r = requests.get(
+        URL,
+        timeout=20
+    )
+
+    texto = BeautifulSoup(
+        r.text,
+        "html.parser"
+    ).get_text().lower()
+
+    palabras = [
+        "solicitud",
+        "plazo abierto",
+        "presentación",
+        "abierto"
+    ]
+
+    for palabra in palabras:
+        if palabra in texto:
+            return True
+
+    return False
+
+
+if comprobar():
+
+    enviar_telegram(
+        "🚗 AUTO+ PUEDE ESTAR ABIERTO\n\n"
+        "Revisa la convocatoria:"
+        "\n" + URL
+    )
+
+else:
+    print("Todavía cerrado")
